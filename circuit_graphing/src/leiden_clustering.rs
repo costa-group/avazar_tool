807698d0a32d91cecfbd032c5c517d6d7026f13f
Alejandro Stuckey de la Banda <paramihijoalejandro@gmail.com>
2026-01-19 17:37:41 +1100

    updated graphrs fix from /4 to /2 as they are dividing by 2 when they shouldn't

--- circuit_graphing/src/leiden_clustering.rs
+++ circuit_graphing/src/leiden_clustering.rs
@@ -35,8 +35,7 @@ impl CanLeiden for RSGraph<usize, usize> {
         // graphrs erroneously divides by 0.5 * m instead of 2 * m ... so we need to divide by 4 to correct this.
 
         // let result = leiden(&self, true, QualityFunction::Modularity, Some(resolution), None, None);
-        let result: Vec<HashSet<usize>> = rs_louvain_communities(&self, true, Some(resolution * 0.25), Some(1e-6), Some(seed)).unwrap(); // fixing a bug in their implementation
-
+        let result: Vec<HashSet<usize>> = rs_louvain_communities(&self, true, Some(resolution * 0.5), Some(1e-6), Some(seed)).unwrap(); // fixing a bug in their implementation
 
         result.into_iter().map(|set| set.into_iter().collect()).collect()
     }
