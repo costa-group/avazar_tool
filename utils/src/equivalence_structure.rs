c403bc4024d10952c9a8a51ba9996d581fa06219
clararod9 <clarrodr@ucm.es>
2026-05-15 10:44:25 +0200

    adding missing files

---
+++ utils/src/equivalence_structure.rs
@@ -0,0 +1,152 @@
+use serde::{Serialize,Deserialize};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use std::error::Error;
+use std::ops::AddAssign;
+use circom_algebra::algebra::Constraint;
+
+#[derive(Deserialize,Serialize, Debug)]
+pub struct TimingInfo{
+    pub graph_construction: Option<f32>,
+    pub clustering: f32,
+    pub dag_construction: f32,
+    pub equivalency: f32,
+    pub total: f32,
+}
+
+impl AddAssign for TimingInfo {
+    
+    fn add_assign(&mut self, other: Self) -> () {
+        self.graph_construction = self.graph_construction.map(|x| other.graph_construction.map(|y| x + y)).flatten();
+        self.clustering += other.clustering;
+        self.dag_construction += other.dag_construction;
+        self.equivalency += other.equivalency;
+        self.total += other.total;
+    }
+} 
+
+#[derive(Deserialize,Serialize, Debug, Clone)]
+pub struct NodeInfo{
+    pub node_id: usize,
+    pub node_name:String,
+    pub constraints_1: Vec<usize>, //ids of the constraints
+    pub constraints_2: Vec<usize>, //ids of the constraints
+    pub input_signals_1: Vec<usize>,
+    pub input_signals_2: Vec<usize>,
+    pub output_signals_1: Vec<usize>,
+    pub output_signals_2: Vec<usize>,
+    pub signals_1: Vec<usize>, 
+    pub signals_2: Vec<usize>, 
+    pub is_custom: bool,
+    pub predecessors: Vec<usize>, //ids of the predecessors
+    pub successors: Vec<usize> //ids of the successors 
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct EquivalenceStructureInfo {
+    pub timing: TimingInfo,
+    pub nodes: Vec<NodeInfo>, //all the nodes of the circuit, position of the node is not the position.
+    pub local_equivalency: Vec<Vec<usize>>, //equivalence classes, each inner vector is a class
+    pub structural_equivalency: Vec<Vec<usize>>, //equivalence classes, each inner vector is a class
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct StructureReader {
+    pub timing: TimingInfo,
+    pub nodes: Vec<NodeInfo>, //all the nodes of the circuit, position of the node is not the position.
+    pub equivalency_local: Option<Vec<Vec<usize>>>, //equivalence classes, each inner vector is a class
+    pub equivalency_structural: Option<Vec<Vec<usize>>>, //equivalence classes, each inner vector is a class
+}
+
+pub struct WeightedArcs<T> {
+    pub original_nodes: Vec<T>,
+    pub arcs: Vec<(T, T, f64)>
+}
+
+
+pub fn read_equivalence_structure<P: AsRef<Path>>(path: P) -> Result<EquivalenceStructureInfo, Box<dyn Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `StructureInfo`.
+    let u: StructureReader = serde_json::from_reader(reader)?;
+
+    Ok(transform_structure_reader(u))
+}
+
+pub fn transform_structure_reader(
+    u: StructureReader
+) -> EquivalenceStructureInfo{
+    let mut local_equivalence = Vec::new();
+    if u.equivalency_local.is_some() { 
+    	local_equivalence = u.equivalency_local.unwrap(); 
+    } else{
+    	// in case no equivalence is given then each node is equivalent to itself only
+    	for n in &u.nodes{
+    		local_equivalence.push(vec![n.node_id]);
+    	}
+    }
+
+    let structural_equivalence;
+    if u.equivalency_structural.is_some() { 
+    	structural_equivalence = u.equivalency_structural.unwrap(); 
+    } else { 
+    	structural_equivalence = local_equivalence.clone();
+    }
+
+    EquivalenceStructureInfo {
+        timing: u.timing,
+        nodes: u.nodes,
+        local_equivalency: local_equivalence,
+        structural_equivalency: structural_equivalence,
+    }
+
+}
+
+
+pub fn generate_empty_equivalence_structure(
+    n_constraints: usize, 
+    n_constraints_2: usize,
+    n_signals:usize,
+    n_signals_2:usize,
+    n_outputs: usize,
+    n_outputs_2: usize,
+    n_inputs: usize,
+    n_inputs_2: usize
+) -> EquivalenceStructureInfo{
+    
+
+    let aux_timing = TimingInfo{
+        clustering: 0.0,
+        graph_construction: Some(0.0),
+        dag_construction: 0.0,
+        equivalency: 0.0,
+        total: 0.0
+    };
+
+    let node = NodeInfo{
+        node_name: "main".to_string(),
+        node_id: 0,
+        constraints_1: (0..n_constraints).collect(),
+        constraints_2: (0..n_constraints_2).collect(),
+        output_signals_1: (1.. n_outputs + 1).collect(),
+        output_signals_2: (1.. n_outputs_2 + 1).collect(),
+        input_signals_1: (n_outputs + 1..n_outputs + n_inputs + 1).collect(),
+        input_signals_2: (n_outputs_2 + 1..n_outputs_2 + n_inputs_2 + 1).collect(),
+        signals_1: (1..n_signals).collect(),
+        signals_2: (1..n_signals_2).collect(),
+        is_custom: false,
+        predecessors: vec![],
+        successors: vec![]
+    };
+    EquivalenceStructureInfo{
+        timing: aux_timing,
+        nodes: vec![node],
+        local_equivalency: vec![vec![0]],
+        structural_equivalency: vec![vec![0]],
+
+    }
+   
+}
---
+++ zk-genver/src/equivalence/modular_reasoning.rs
@@ -0,0 +1,326 @@
+use solvers_interface::{EquivalenceVerification, PossibleResult, PossibleSolver, cvc5_interface, ffsol_interface, nia_z3_interface, parallel_interface, yices_interface, z3_interface};
+type Constraint = circom_algebra::algebra::Constraint<usize>;
+use circom_algebra::num_bigint::BigInt;
+use std::collections::LinkedList;
+use std::time::{Instant, Duration};
+use utils::equivalence_structure::NodeInfo;
+use std::collections::{HashSet,HashMap};
+use crate::equivalence::equivalence_check::ResultInfoEquivalence;
+
+
+pub type EquivalenceImplication = (Vec<(usize, usize)>, Vec<(usize, usize)>);
+
+    pub fn check_node(
+        node_info: &NodeInfo, 
+        field: &BigInt, 
+        verification_timeout: u64,
+        node_list: &Vec<NodeInfo>,
+        nodeid2pos: &HashMap<usize, usize>,
+        constraint_list_1: &Vec<Constraint>,
+        constraint_list_2: &Vec<Constraint>,
+        solver: PossibleSolver,
+        apply_deduction_assigned: bool,
+        include_niaz3_in_all: bool,
+        apply_predecessors:bool,
+        apply_bidirectional: bool,
+        no_abstract_fails:bool,
+        results:&ResultInfoEquivalence,
+        extra_rounds: usize,
+        verbose: bool
+    ) 
+    -> (PossibleResult, f64, usize, bool, Vec<String>, HashSet<usize>){
+        
+        let signals_1: LinkedList<usize> = node_info.signals_1.clone().into_iter().collect(); 
+        let signals_2: LinkedList<usize> = node_info.signals_2.clone().into_iter().collect(); 
+
+        let mut constraints_1 = Vec::new();
+        for c in &node_info.constraints_1{
+            constraints_1.push(constraint_list_1[*c].clone());
+        }
+        let mut constraints_2 = Vec::new();
+        for c in &node_info.constraints_2{
+            constraints_2.push(constraint_list_2[*c].clone());
+        }
+
+        let mut logs =  Vec::new();
+        let mut n_rounds = 0;
+        let mut unknown_rounds = 0;
+        let implications_safety: Vec<EquivalenceImplication> = Vec::new();
+
+
+        let mut verification = EquivalenceVerification::new(
+            &node_info.node_name.to_string(), 
+            signals_1,
+            signals_2, 
+            node_info.input_signals_1.clone(),
+            node_info.input_signals_2.clone(),
+            node_info.output_signals_1.clone(),
+            node_info.output_signals_2.clone(),
+            constraints_1.clone(), 
+            constraints_2.clone(),
+            implications_safety,
+            field,
+            verification_timeout,
+            apply_deduction_assigned,
+            verbose
+        );
+
+        let mut to_check_next=Vec::new();
+        if !apply_predecessors || apply_bidirectional{
+            let mut to_check = generate_and_add_node_info(&node_info.successors, &mut verification, node_list, nodeid2pos, constraint_list_1, constraint_list_2, results, apply_bidirectional, no_abstract_fails);
+            to_check_next.append(&mut to_check);
+        } 
+        if apply_predecessors || apply_bidirectional{
+            let mut to_check = generate_and_add_node_info(&node_info.predecessors, &mut verification, node_list, nodeid2pos, constraint_list_1, constraint_list_2, results, apply_bidirectional, false);
+            to_check_next.append(&mut to_check);
+        }
+
+        
+        logs.push(format!("Checking template {}\n", node_info.node_id));
+        logs.push(format!("Number of signals in the first version (i,int,o): {}\n", node_info.signals_1.len()));      
+        logs.push(format!("Number of signals in the second version (i,int,o): {}\n", node_info.signals_2.len()));      
+
+        logs.push(format!("Number of constraints in the first template: {}\n", node_info.constraints_1.len()));
+        logs.push(format!("Number of constraints in the second template: {}\n", node_info.constraints_2.len()));
+
+        let inicio = Instant::now();
+
+        let (mut result_safety, mut logs_round) = prove_equivalence(&verification, solver);
+
+        let mut used_extra_rounds = false;
+        let mut finished_verification = match result_safety{
+            PossibleResult::UNKNOWN =>{
+                unknown_rounds += 1;
+                if unknown_rounds <= extra_rounds {
+                    used_extra_rounds = true;
+                }
+                unknown_rounds > extra_rounds
+            },
+            PossibleResult::FAILED =>{
+                false
+            },
+            _ => true
+        };
+        logs.append(&mut logs_round);
+        
+        while !finished_verification && !to_check_next.is_empty(){
+            n_rounds += 1;
+
+            let new_components = std::mem::take(&mut to_check_next);
+            for node_id in &new_components{
+                if *node_id != node_info.node_id && !verification.added_nodes.contains(node_id) { 
+
+                    let pos = nodeid2pos[node_id];
+                    let node = &node_list[pos];
+                    let result_add_components = add_info_component(node, &mut verification, node_list, nodeid2pos, constraint_list_1, constraint_list_2, results, apply_predecessors, apply_bidirectional, no_abstract_fails);                    
+                    if result_add_components.is_some(){
+                        to_check_next.append(&mut result_add_components.unwrap());
+                    }
+                    verification.added_nodes.insert(*node_id);
+                }
+            }
+ 
+
+            logs.push(format!("### Trying to verify adding constraints of the children\n"));
+            (result_safety, logs_round) = prove_equivalence(&verification, solver);
+            finished_verification = match result_safety{
+                PossibleResult::UNKNOWN =>{
+                    unknown_rounds += 1;
+                    if unknown_rounds <= extra_rounds {
+                        used_extra_rounds = true;
+                    }
+                    unknown_rounds > extra_rounds
+                },
+                PossibleResult::FAILED =>{
+                    false
+                },
+                _ => true
+            };
+            logs.append(&mut logs_round);
+
+        } 
+        let duration = inicio.elapsed();  
+        pretty_print_result(&mut logs, duration, n_rounds, &result_safety);
+        let extra_rounds_helped = used_extra_rounds && result_safety == PossibleResult::VERIFIED;
+        (
+            result_safety,
+            duration.as_secs_f64(),
+            n_rounds,
+            extra_rounds_helped,
+            logs,
+            verification.added_nodes,
+        )
+        
+    }
+
+    fn add_info_component(
+        info: &NodeInfo, 
+        verification: &mut EquivalenceVerification, 
+        node_list: &Vec<NodeInfo>, 
+        nodeid2pos: &HashMap<usize, usize>, 
+        constraint_list_1: &Vec<Constraint>,
+        constraint_list_2: &Vec<Constraint>,
+        results:&ResultInfoEquivalence,
+        apply_predecessors: bool,
+        apply_bidirectional: bool,
+        no_abstract_fails: bool
+    )-> Option<Vec<usize>>{
+
+            for c in &info.constraints_1{
+                verification.constraints_1.push(constraint_list_1[*c].clone());
+            }
+            for s in &info.signals_1{
+                verification.signals_1.push_back(*s);
+            }
+            for c in &info.constraints_2{
+                verification.constraints_2.push(constraint_list_2[*c].clone());
+            }
+            for s in &info.signals_2{
+                verification.signals_2.push_back(*s);
+            }
+            let mut to_check_next: Vec<usize> = Vec::new();
+            if !apply_predecessors || apply_bidirectional{
+                let mut to_check = generate_and_add_node_info(&info.successors, verification, node_list, nodeid2pos, constraint_list_1, constraint_list_2,  results, apply_bidirectional, no_abstract_fails);
+                to_check_next.append(&mut to_check);
+            } 
+            if apply_predecessors || apply_bidirectional{
+                            //println!("Entra pred");
+
+                let mut to_check = generate_and_add_node_info(&info.predecessors, verification, node_list, nodeid2pos, constraint_list_1, constraint_list_2, results, apply_bidirectional, false);
+                to_check_next.append(&mut to_check);
+            }
+
+            if to_check_next.len() > 0 {Some(to_check_next)} else {None}
+    }
+
+    fn generate_and_add_node_info(
+        node_ids: &[usize], 
+        verification: &mut EquivalenceVerification, 
+        node_list: &Vec<NodeInfo>, 
+        nodeid2pos: &HashMap<usize, usize>, 
+        constraint_list_1: &Vec<Constraint>,
+        constraint_list_2: &Vec<Constraint>,
+        results:&ResultInfoEquivalence, 
+        apply_bidirectional: bool,
+        no_abstract_fails: bool,
+    ) -> Vec<usize> {
+        let mut to_check_next = Vec::new();
+        for node_id in node_ids {
+            println!("node_id {}", node_id);
+            let pos = nodeid2pos[node_id];
+            let subtree_child = &node_list[pos];
+            let (mut new_signals_1, mut new_signals_2, new_implications_safety) = generate_info_subtree(subtree_child);
+            verification.signals_1.append(&mut new_signals_1);
+            verification.signals_2.append(&mut new_signals_2);
+
+            if no_abstract_fails && results.studied_nodes.contains_key(node_id){
+                    let result = results.studied_nodes.get(node_id).unwrap();
+                    match result{
+                        PossibleResult::VERIFIED => {
+                            verification.implications_equivalence.push(new_implications_safety);
+                            to_check_next.push(*node_id);
+                        }
+                        _ =>{
+                            if !verification.added_nodes.contains(node_id) { 
+                                let pos = nodeid2pos[node_id];
+                                let node = &node_list[pos];
+                                let result_add_components = add_info_component(node, verification, node_list, nodeid2pos, constraint_list_1, constraint_list_2, results,  false, false, no_abstract_fails);                    
+                                if result_add_components.is_some(){
+                                    for aux in result_add_components.unwrap(){
+                                        to_check_next.push(aux);
+                                    }
+                                }
+                                verification.added_nodes.insert(*node_id);
+                            }
+                        }
+                    }
+            }else{
+                verification.implications_equivalence.push(new_implications_safety);
+                to_check_next.push(*node_id);
+            }            
+            
+        }
+        to_check_next
+    }
+
+    fn generate_info_subtree(info: &NodeInfo)-> (LinkedList<usize>, LinkedList<usize>, EquivalenceImplication){
+        let (io_signals_1, io_signals_2) = generate_io_signals(info);
+        ( 
+            io_signals_1,
+            io_signals_2,
+            generate_implications_safety(info)
+        )
+    }
+
+    fn generate_io_signals(info: &NodeInfo)-> (LinkedList<usize>, LinkedList<usize>){
+        let mut signals_1 = LinkedList::new();
+        for s in &info.input_signals_1{
+            signals_1.push_back(*s);
+        }
+        for s in &info.output_signals_1{
+            signals_1.push_back(*s);
+        }  
+        let mut signals_2 = LinkedList::new();
+        for s in &info.input_signals_2{
+            signals_2.push_back(*s);
+        }
+        for s in &info.output_signals_2{
+            signals_2.push_back(*s);
+        }  
+        (signals_1, signals_2)
+    }
+    
+    fn generate_implications_safety(info: &NodeInfo)-> EquivalenceImplication{
+        let mut list_inputs = Vec::new();
+        let mut list_outputs = Vec::new();
+
+        for i in 0..info.output_signals_1.len(){
+            list_outputs.push((info.output_signals_1[i], info.output_signals_2[i]));
+        }
+        for i in 0..info.input_signals_1.len(){
+            list_inputs.push((info.input_signals_1[i], info.input_signals_2[i]));
+        }
+        (list_inputs, list_outputs)
+    }
+
+    fn pretty_print_result(logs: &mut Vec<String>, duration: Duration, n_rounds: usize, result: &PossibleResult){
+        logs.push(format!("Verification time per template: {}\n", duration.as_secs_f64()));    
+        logs.push(format!("     NUMBER OF ROUNDS: {}\n\n ", n_rounds));
+        logs.push(format!("******** VERIFICATION RESULTS ********\n"));
+
+        logs.push(format!("-----> WEAK SAFETY: "));
+        logs.push(result.result_to_str());
+
+        logs.push(format!("\n\n"));
+    }
+
+
+
+    fn prove_equivalence(
+        problem: &EquivalenceVerification,
+        solver: PossibleSolver,
+    )-> (PossibleResult, Vec<String>) {
+        match solver{
+            PossibleSolver::FFSOL=>{
+                ffsol_interface::study_equivalence(
+                    problem,
+                    &ffsol_interface::FfsolConfig::default(problem.verification_timeout, problem.verbose),
+                )
+            },
+            PossibleSolver::CVC5=>{
+                cvc5_interface::study_equivalence(problem)
+            },
+            PossibleSolver::YICES=>{
+                yices_interface::study_equivalence(problem)
+            },
+            PossibleSolver::NIAZ3=>{
+                nia_z3_interface::study_equivalence(problem)
+            },
+            PossibleSolver::Z3=>{
+                z3_interface::study_equivalence(problem)
+            },
+            _ => unreachable!()
+        }
+    }
+
+
