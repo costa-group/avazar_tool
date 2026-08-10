9e2b20739baa497c8b044bb3bc2366c4f7059d77
Alejandro Stuckey de la Banda <paramihijoalejandro@gmail.com>
2026-08-10 12:54:44 +1000

    changed resolution fix back to div4, unknown why this was changed, added some comments

--- circuit_graphing/src/leiden_clustering.rs
+++ circuit_graphing/src/leiden_clustering.rs
@@ -7,10 +7,12 @@ use graphrs::algorithms::community::louvain::{louvain_communities_from_weighted_
 use xgraph::Graph as XGraph;
 use xgraph::graph::algorithms::leiden_clustering::{CommunityDetection, CommunityConfig as XCommunityConfig};
 
-
+// Trait to to handle the leiden/louvain calls for the various graph backends
 pub trait CanLeiden {
     fn num_edges(&self) -> usize;
-    fn get_partition(self: Box<Self>, target: f64, max_iterations: usize, seed: Option<u64>) -> Vec<Vec<usize>>;
+
+    // Wrapper call for the various leiden/louvain calls. 
+    fn get_partition(self: Box<Self>, resolution: f64, max_iterations: usize, seed: Option<u64>) -> Vec<Vec<usize>>;
 }
 
 impl CanLeiden for XGraph<f64, (), ()> {
@@ -35,7 +37,7 @@ impl CanLeiden for WeightedArcs<usize> {
     fn get_partition(self: Box<Self>, resolution: f64, _max_iterations: usize, seed: Option<u64>) -> Vec<Vec<usize>> {
         // graphrs erroneously divides by 0.5 * m instead of 2 * m ... so we need to divide by 4 to correct this.
         // NOTE: passing arcs directly is ONLY possible because of the use of the IdentityIndexer, otherwise it need to pass arcs with T mapped to usize by the indexer
-        let result: Vec<HashSet<usize, _>> = rs_louvain_communities(self.original_nodes, self.arcs, IdentityIndexer, true, Some(resolution * 0.5), Some(1e-6), seed).unwrap(); // fixing a bug in their implementation
+        let result: Vec<HashSet<usize, _>> = rs_louvain_communities(self.original_nodes, self.arcs, IdentityIndexer, true, Some(resolution * 0.25), Some(1e-6), seed).unwrap(); // fixing a bug in their implementation
         result.into_iter().map(|set| set.into_iter().collect()).collect()
     }
 }
